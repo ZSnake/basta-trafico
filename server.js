55254b7b0aa664f85ef1e073187ae0cc97e78fac
Viktor Zavala <viktor@acklenavenue.com>
2015-11-07 18:32:14 -0600

    added mongoose configuration for server

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var mongoose = require('mongoose');
 var inert = require('inert');
 var routes = require('./routes');
 // Create a server with a host and port
-var server = new hapi.Server(~~process.env.PORT || 3000, '0.0.0.0');
-// server.connection({
-//     //host: 'localhost',
-//     port: process.env.PORT | 3000,
-// });
+var server = new hapi.Server();
+server.connection({
+    host: '0.0.0.0',
+    port: +process.env.PORT | 3000,
+});
 
-mongoose.connect('mongodb://localhost/bastaTrafico');
+mongoose.connect(process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || 'mongodb://localhost/bastaTrafico');
+    
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
